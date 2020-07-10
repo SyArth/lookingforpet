25db5a68feab0b95f7d6f651af75e6d2b35335a3
SyArth <dev.web56@laposte.net>
2020-07-10 13:17:15 +0200

    mise à jour de l'entité Puce

--- src/Entity/Puce.php
+++ src/Entity/Puce.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\PuceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PuceRepository::class)
@@ -17,32 +19,25 @@ class Puce
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $puceOK;
-
+   
     /**
      * @ORM\OneToOne(targetEntity=Animal::class, inversedBy="puce", cascade={"persist", "remove"})
+     * @UniqueEntity(fields={"numero"}, message="Un animal a déjà ce numero de puce.")
      */
     private $animal;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true, unique=true)
+     * @Assert\Regex(pattern="/^[0-9]{3}[0-9]{2}[0-9]{2}[0-9]{8}$/")
+     */
+    private $numero;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getPuce(): ?bool
-    {
-        return $this->puce;
-    }
-
-    public function setPuce(?bool $puce): self
-    {
-        $this->puce = $puce;
-
-        return $this;
-    }
+    
 
     public function getAnimal(): ?Animal
     {
@@ -52,6 +47,18 @@ class Puce
     public function setAnimal(?Animal $animal): self
     {
         $this->animal = $animal;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(?string $numero): self
+    {
+        $this->numero = $numero;
 
         return $this;
     }
